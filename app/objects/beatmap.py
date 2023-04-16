ff8eac81bd40faae432f11c863c3c4967f15cf74
arily <arily@arily.moe>
2023-04-16 18:32:09 +0900

    comments

--- app/objects/beatmap.py
+++ app/objects/beatmap.py
@@ -70,6 +70,8 @@ async def ensure_local_osu_file(
     downloading it from the osu!api if required."""
 
     exists = osu_file_path.exists()
+    # some callers have type Any | str on "bmap_md5" so I assume it's optional, thus May be None.
+    # I use a magic string here to avoid letting optional md5 slip through.
     file_md5 = hashlib.md5(osu_file_path.read_bytes()).hexdigest() if exists else "MAGIC_STRING__NOT_EXISTS"
     if (file_md5 == bmap_md5):
         return True
@@ -89,8 +91,10 @@ async def ensure_local_osu_file(
         bytes_md5 = hashlib.md5(b_beatmap).hexdigest()
         if (bytes_md5 in KNOWN_BAD_FILES_MD5): 
             return False
+        # at least this' not a known bad file
         osu_file_path.write_bytes(b_beatmap)
-        if (bmap_md5 and bytes_md5 != bytes_md5):
+        # some callers have type Any | str on "bmap_md5" so I assume it's optional
+        if (bmap_md5 and bytes_md5 != bmap_md5):
             return False
         return True
 
