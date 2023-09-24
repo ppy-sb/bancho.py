f3c7b690f525a73be146c3b7e524ffc43d4e004c
Usagi no Niku <chenbohan911@163.com>
2023-09-24 15:48:42 +0800

    revert changes in upstream/apiv2-oauth

--- app/api/v2/common/json.py
+++ app/api/v2/common/json.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from typing import Any
-from uuid import UUID
 
 import orjson
 from fastapi.responses import JSONResponse
@@ -15,8 +14,6 @@ def _default_processor(data: Any) -> Any:
         return {k: _default_processor(v) for k, v in data.items()}
     elif isinstance(data, list):
         return [_default_processor(v) for v in data]
-    elif isinstance(data, UUID):
-        return str(data)
     else:
         return data
 
@@ -25,12 +22,8 @@ def dumps(data: Any) -> bytes:
     return orjson.dumps(data, default=_default_processor)
 
 
-def loads(data: str) -> Any:
-    return orjson.loads(data)
-
-
 class ORJSONResponse(JSONResponse):
-    media_type = "application/json;charset=UTF-8"
+    media_type = "application/json"
 
     def render(self, content: Any) -> bytes:
         return dumps(content)
