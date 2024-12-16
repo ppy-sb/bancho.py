1e18ff454d83e162ebf87268716979be252edcc9
Usagi no Niku <chenbohan911@163.com>
2024-12-16 07:31:17 +0000

    fix: fix bad assigned of sb api

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,11 +5,13 @@ from fastapi import APIRouter
 
 from .v1 import apiv1_router
 from .v2 import apiv2_router
+from ..sb.sb_api import router as sb_router
 
 api_router = APIRouter()
 
 api_router.include_router(apiv1_router)
 api_router.include_router(apiv2_router)
+api_router.include_router(sb_router)
 
 from . import domains
 from . import init_api
